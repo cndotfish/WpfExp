431b8f215a27a128b139f2e719bbe4aa0b1d0489
wxr <543164364@qq.com>
2013-03-01 11:43:01 +0800

    测试Xml读写

---
+++ TestXmlRead.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfExp
+{
+    public class TestXmlRead
+    {
+        public static void ReadXmlByXmlDocument(string filePath)
+        {
+
+        }
+
+        public static void ReadXmlByXmlReader(string filePath)
+        {
+
+        }
+
+        public static void ReadXmlBySerialize(string filePath)
+        {
+
+        }
+
+        public static void ReadXmlByLINQ(string filePath)
+        {
+
+        }
+
+    }
+}
